9525117afb4bb775a5059205493c6d45d1d56549
odilo <odilomurindahabi@gmail.com>
2024-07-01 17:52:09 +0200

    install prisma

--- api/app.js
+++ api/app.js
@@ -2,8 +2,9 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 
 const app = express();
+app.use(express.json());
 
-app.use("/api/user", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(8800, () => {
   console.log("server is running on port 8800")
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,12 @@
-export const register = (req, res) => {
+import bcrypt from 'bcrypt'
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
   //db operations
+  res.json({username,email,password})
 }
 export const login = (req, res) => {
   //db operations
