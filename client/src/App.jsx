0140bb4ac042add87d9a4970dee5a4bbb1000d94
odilo <odilomurindahabi@gmail.com>
2024-07-02 10:37:39 +0200

    create sideBar menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import Navbar from "./components/navBar/Navbar";
+import SidebarMenu from "./components/sidebarMenu/SidebarMenu";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const changeVisibility = (option) => {
+    setIsOpen(option);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		<div className='layout   lg:px-32 md:px-16 px-5 relative'>
+			<div className='bg-orange-100 h-svh'>
+				<Navbar changeVisibility={changeVisibility} />
+				{isOpen && <SidebarMenu changeVisibility={changeVisibility} />}
+			</div>
+		</div>
+	);
 }
-
-export default App
---
+++ client/src/components/navBar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Logo from '../../assets/logo11.png'
+import { CgMenuRound } from "react-icons/cg";
+
+const Navbar = ({ changeVisibility }) => {
+	return (
+		<div className='flex justify-between px-2 pt-1 items-center'>
+			<div className='flex items-center '>
+				<div className='flex items-center gap-3'>
+					<img src={Logo} alt='logo' className='md:w-[45px] w-[35px]' />
+					<p className='font-bold lg:flex hidden'>UnlimitedEstate</p>
+				</div>
+			</div>
+			<ul className='md:flex items-center gap-10 hidden'>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+				<li>Agents</li>
+			</ul>
+
+			<div>
+				<ul className='md:flex items-center justify-end gap-5 hidden'>
+					<li>Log In</li>
+					<li className='bg-orange-300 px-5 py-2 rounded'>Sign Up</li>
+				</ul>
+			</div>
+			<div className='md:hidden flex'>
+				<CgMenuRound className='text-2xl' onClick={()=>changeVisibility(true)}/>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar
